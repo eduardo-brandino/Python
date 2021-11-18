93f743659b40e72914fa52d95dab019b8a32c8c5
eduardo-brandino <eduardo.brandino@fatecitapetininga.edu.br>
2021-11-18 13:22:23 -0300

    URI - 1013

--- Python/URI - 1013.py
+++ Python/URI - 1013.py
@@ -1,17 +1,4 @@
-'''Faça um programa que leia três valores e apresente o maior dos três valores
-lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
-MaiorAB= \frac{\left(a+b+abs\left(a-b\right)\right)}{2}
-
-
-Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b).
-Um segundo passo, portanto é necessário para chegar no resultado esperado.
-
-Entrada
-O arquivo de entrada contém três valores inteiros.
-
-Saída
-Imprima o maior dos três valores seguido por um espaço e a mensagem
- "eh o maior".'''
+#https://www.beecrowd.com.br/judge/pt/problems/view/1013
 
 x = input().split()
 a, b, c = x
