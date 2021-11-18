1275ec00dfd125bdc1dd39f2f3aff001357635f8
eduardo-brandino <eduardo.brandino@fatecitapetininga.edu.br>
2021-11-17 23:33:07 -0300

    URI - 1012

---
+++ Python/URI - 1012.py
@@ -0,0 +1,32 @@
+'''Escreva um programa que leia três valores com ponto flutuante
+ de dupla precisão: A, B e C. Em seguida, calcule e mostre:
+a) a área do triângulo retângulo que tem A por base e C por altura.
+b) a área do círculo de raio C. (pi = 3.14159)
+c) a área do trapézio que tem A e B por bases e C por altura.
+d) a área do quadrado que tem lado B.
+e) a área do retângulo que tem lados A e B.
+Entrada
+O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+
+Saída
+O arquivo de saída deverá conter 5 linhas de dados.
+Cada linha corresponde a uma das áreas descritas acima,
+sempre com mensagem correspondente e um espaço entre os dois pontos e o valor.
+O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+'''
+
+x =input().split()
+a, b, c = x
+pi = 3.14159
+
+tri = ((float(a) * float(c) )/ 2)
+cir = (pi * float(c)**2)
+tra = (float(c) * ((float(a) + float(b)) / 2))
+qua = (float(b)**2)
+ret = (float(a) * float(b))
+
+print('TRIANGULO: {:.3f}'.format(tri))
+print('CIRCULO: {:.3f}'.format(cir))
+print('TRAPEZIO: {:.3f}'.format(tra))
+print('QUADRADO: {:.3f}'.format(qua))
+print('RETANGULO: {:.3f}'.format(ret))
