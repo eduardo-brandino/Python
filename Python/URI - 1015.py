7ff9791a1d4a25db46ef6f95e3b1f9ef5bc8d38b
eduardo-brandino <eduardo.brandino@fatecitapetininga.edu.br>
2021-11-18 13:23:31 -0300

    URI - 1015

--- Python/URI - 1015.py
+++ Python/URI - 1015.py
@@ -1,18 +1,5 @@
-'''Leia os quatro valores correspondentes aos eixos x e y de dois pontos
-quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles,
-mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+#https://www.beecrowd.com.br/judge/pt/problems/view/1015
 
-Distancia =raizde(x2-x1)²+(y2-y1)²
-
-Entrada
-O arquivo de entrada contém duas linhas de dados.
-A primeira linha contém dois valores de ponto flutuante: x1 y1 e a
-segunda linha contém dois valores de ponto flutuante x2 y2.
-
-Saída
-Calcule e imprima o valor da distância segundo a fórmula fornecida,
-com 4 casas após o ponto decimal.
-'''
 p1 = input().split()
 x1, y1 = p1
 p2 = input().split()
