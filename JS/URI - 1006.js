733bb31c8065c414e88fe398c987972cb420812b
eduardo-brandino <eduardo.brandino@fatecitapetininga.edu.br>
2021-11-18 13:55:31 -0300

    URI + 1006 js

---
+++ JS/URI - 1006.js
@@ -0,0 +1,13 @@
+#https://www.beecrowd.com.br/judge/pt/problems/view/1006
+
+var input = require("fs").readFileSync("stdin", "utf8");
+
+var linhas = input.split("\n");
+
+var A = parseFloat(linhas.shift());
+var B = parseFloat(linhas.shift());
+var C = parseFloat(linhas.shift());
+
+var media = (A*2 + B*3 + C*5) / 10.0;
+
+console.log("MEDIA = " + media.toFixed(1));
